2b4622a021f182a2655d4f9812b0d14b4f914a4e
Lolnani-Automata <overlordzero000@gmail.com>
2020-01-10 10:14:46 -0800

    daftar member fix

--- application/views/front-end/daftar/member.php
+++ application/views/front-end/daftar/member.php
@@ -13,21 +13,21 @@
     <!-- Main css -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>app-assets/form/css/style.css">
 
-    <style>
-    #hapus {
-                display: none;
-                position: fixed;
-                width: 1190px;
-                bottom: 0;
-                left: 6%;
-                z-index: 99;
-                border: none;
-                outline: none;
-                cursor: default;
-            }
-    </style>
 <script type="text/javascript"> 
     
+function p1(){
+    var num_acc1= document.getElementById('vs1').value;
+    document.getElementById('acc').value = num_acc1;
+}
+function p2(){
+    var num_acc2= document.getElementById('pp1').value;
+    document.getElementById('acc').value = num_acc2;
+}
+function p3(){
+    var num_acc3= document.getElementById('mc1').value;
+    document.getElementById('acc').value = num_acc3;
+}
+
     function save(){
         $('#btnSave').text('Saving...'); //change button text
         $('#btnSave').attr('disabled',true); //set button disable 
@@ -66,7 +66,7 @@
 </script>
 
 </head>
-<body style=" overflow: hidden;">
+<body style=" overflow: hidden;" >
 
     <div class="main" style="padding: 20px 0;">
         <!-- Sing in  Form -->
@@ -110,7 +110,7 @@
                             <div class="form-group form-button" >
                                 <a href="#two"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;" value="Lanjutkan"/></a>
 
-                                <input type="hidden" id="btnSave" class="form-submit" style="margin-top:unset;" value="Simpan" onclick="save();"/>
+                                
                             </div>
                         </form>
                     </div>
@@ -118,41 +118,210 @@
             </div>
         </section>
 
-        <section class="sign-in" id="two" style="margin-bottom: 100px;">
-            <div class="container" style="width: 900px; height:460px;">
-            <h2 style="text-align: center; padding-top:25px;">Paket Promo Apalah</h2>
-                <div class="signin-content" >
-                    <div class="jarak">
-                        <div class="social-login">
-                            <ul class="socials">
-                                <li><a href="#" onclick="pilih('<?php $p1 = 'PKT001'; echo $p1; ?>')"><i class="display-flex-center zmdi zmdi-facebook" style="width: 200px; height:0px; content:none;"><img src="<?php echo base_url(); ?>assets/front-end/images/undraw_laravel_and_vue_59tp.svg" alt="sing up image"></i></a>
-                                <h3 style="text-align: center; margin-top:130px;">Pilih Paket <br> <small>$00.00</small> </h3>
-                            </li>
-                            </ul>
+        <section class="sign-in" id="two" style="margin-bottom: 100px; padding-top:50px;">
+            <div class="container" style="background-color: unset; box-shadow:none;">
+            <div class="row" style="display: inline-flex;margin-left: -50px;">
+                <div class="col-md-4">
+                    <div class="container" style="width: 300px; margin:20px;">
+                        <div class="signin-content" style="padding-top:55px;">
+                            <div class="jarak">
+                                <div class="social-login" style="margin-top: unset;">
+                                    <ul class="socials">
+                                        <span class="price">
+                                            <span class="number" style="font-size: 35pt;padding: 60px ;">0<small style="font-size:8pt;" class="per">/mo</small>
+                                        </span><br>
+                                        <span style="color: #b3b3b3;font-size: large;">100% free. Forever</span>
+                                        <h3 class="heading-2 mb-3">Enjoy All The Features ok</h3>
+                                        
+                                        <ul class="pricing-text mb-4">
+                                        <li><strong>150 GB</strong> Bandwidth</li>
+                                        <li><strong>100 GB</strong> Storage</li>
+                                        <li><strong>$1.00 / GB</strong> Overages</li>
+                                        <li>All features</li>
+                                        </ul>
+                                        <a href="#tho" onclick="pilih('<?php $p1 ='PKT001'; echo $p1; ?>')"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;" value="Lanjutkan"/></a>
+</li>
+                                    </ul>
+                                </div>
+                            </div>
+                                    
                         </div>
                     </div>
-                    <div class="jarak">
-                        <div class="social-login">
-                            <ul class="socials">
-                                <li><a href="#" onclick="pilih('<?php $p2 = 'PKT002'; echo $p2; ?>')"><i class="display-flex-center zmdi zmdi-facebook" style="width: 200px; height:0px; content:none;"><img src="<?php echo base_url(); ?>assets/front-end/images/undraw_visual_data_b1wx.svg" alt="sing up image"></i></a>
-                                <h3 style="text-align: center; margin-top:130px;">Pilih Paket<br> <small>$00.00</small></h3>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                </div>
+                <div class="col-md-4">
+                <div class="container" style="width: 300px; margin:20px;">
+                        <div class="signin-content" style="padding-top:55px;">
+                            <div class="jarak">
+                                <div class="social-login" style="margin-top: unset;">
+                                    <ul class="socials">
+                                        <span class="price">
+                                            <span class="number" style="font-size: 35pt;padding: 60px ;">0<small style="font-size:8pt;" class="per">/mo</small>
+                                        </span><br>
+                                        <span style="color: #b3b3b3;font-size: large;">100% free. Forever</span>
+                                        <h3 class="heading-2 mb-3">Enjoy All The Features</h3>
+                                        
+                                        <ul class="pricing-text mb-4">
+                                        <li><strong>150 GB</strong> Bandwidth</li>
+                                        <li><strong>100 GB</strong> Storage</li>
+                                        <li><strong>$1.00 / GB</strong> Overages</li>
+                                        <li>All features</li>
+                                        </ul>
+                                        <a href="#tho" onclick="pilih('<?php $p1 ='PKT001'; echo $p1; ?>')"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;" value="Lanjutkan"/></a>
+</li>
+                                    </ul>
+                                </div>
+                            </div>
 
-                    <div class="jarak">
-                        <div class="social-login">
-                            <ul class="socials">
-                                <li><a href="#" onclick="pilih('<?php $p3 = 'PKT003'; echo $p3; ?>')"><i class="display-flex-center zmdi zmdi-facebook" style="width: 200px; height:0px; content:none;"><img src="<?php echo base_url(); ?>assets/front-end/images/undraw_business_plan_5i9d.svg" alt="sing up image"></i></a>
-                                <h3 style="text-align: center; margin-top:130px;">Pilih Paket<br> <small>$00.00</small></h3>
-                                </li>
-                            </ul>
                         </div>
                     </div>
-                    
+                </div>
+                <div class="col-md-4">
+                <div class="container" style="width: 300px; margin:20px;">
+                        <div class="signin-content" style="padding-top:55px;">
+                            <div class="jarak">
+                                <div class="social-login" style="margin-top: unset;">
+                                    <ul class="socials">
+                                        <span class="price">
+                                            <span class="number" style="font-size: 35pt;padding: 60px ;">0<small style="font-size:8pt;" class="per">/mo</small>
+                                        </span><br>
+                                        <span style="color: #b3b3b3;font-size: large;">100% free. Forever</span>
+                                        <h3 class="heading-2 mb-3">Enjoy All The Features</h3>
+                                        
+                                        <ul class="pricing-text mb-4">
+                                        <li><strong>150 GB</strong> Bandwidth</li>
+                                        <li><strong>100 GB</strong> Storage</li>
+                                        <li><strong>$1.00 / GB</strong> Overages</li>
+                                        <li>All features</li>
+                                        </ul>
+                                        <a href="#tho" onclick="pilih('<?php $p1 ='PKT001'; echo $p1; ?>')"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;" value="Lanjutkan"/></a>
+
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                                    
+                        </div>
+                    </div>
                 </div>
             </div>
+            </div>
+        </section>
+
+
+
+
+
+
+        
+        <section class="sign-in" id="tho" style="margin-bottom: 100px; padding-top:50px;margin-right:275px;">
+
+        <div class="container" style="background-color: unset; box-shadow:none;">
+            <div class="row" style="display: inline-flex;margin-left: -50px;">
+                <div class="col-md-4">
+                    <div class="container" style="width: 400px; margin:20px;">
+                        <div class="signin-content" style="padding-top:55px;">
+                            <div class="jarak">
+                            <img src="<?php echo base_url(); ?>app-assets/form/images/visa.jpg" alt="sing up image" style="width:100px;height:100px;margin-left:30px">
+                                <div class="social-login" style="margin-top: unset;">
+
+                                        <div class="form-group">
+                                            
+                                            <input id="vs1" type="email"  placeholder="Account Number"/>
+                            
+                                            
+                                        </div>
+
+                                </div>
+                                <br>
+                                <a href="#thu" onclick="p1()"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;margin-left:45px" value="Lanjutkan" /></a>
+
+                            </div>
+                                    
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                <div class="container" style="width: 400px; margin:20px;">
+                        <div class="signin-content" style="padding-top:55px;">
+                            <div class="jarak">
+                            <img src="<?php echo base_url(); ?>app-assets/form/images/paypal.jpg" alt="sing up image" style="width:100px;height:100px;margin-left:30px">
+                                <div class="social-login" style="margin-top: unset;">
+
+                                <div class="form-group">
+                                            
+                                            <input id="pp1" type="email"  placeholder="Account Number"/>
+                                
+                                        </div>
+
+                                </div>
+                                <br>
+                                <a href="#thu" onclick="p2()"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;margin-left:45px" value="Lanjutkan"/></a>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                <div class="container" style="width: 400px; margin:20px;">
+                        <div class="signin-content" style="padding-top:55px;">
+                            <div class="jarak">
+                            <img src="<?php echo base_url(); ?>app-assets/form/images/master.jpg" alt="sing up image" style="width:100px;height:100px;margin-left:30px">
+                                <div class="social-login" style="margin-top: unset;">
+
+                                <div class="form-group">
+                                            
+                                            <input id="mc1" type="email"  placeholder="Account Number"/>
+                            
+                                        </div>
+
+                                </div>
+
+                                <br>
+                                <a href="#thu" onclick="p3()"><input type="button" name="signup" id="signup" class="form-submit" style="margin-top:unset;margin-left:45px" value="Lanjutkan"/></a>
+
+                            </div>
+                                    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        
+        </section>
+
+        <section class="sign-in" id="thu" style="margin-bottom: 100px; padding-top:50px;margin-right:275px;">
+        
+<div class="container" style="margin-left:300px">
+<div class="row">
+<div class="col-sm-8" style="padding: 55px">
+
+<div style="font-size: 25pt">Account Number</div>
+<div class="form-group">                                            
+<input style="font-size: 15pt" type="email"  id="acc"/>
+</div>
+<div style="font-size: 25pt">Jumlah Nominal</div>
+<div class="form-group">                                            
+<input style="font-size: 15pt" type="email"  placeholder="Rp.500.000"/>
+<br>
+<br>
+<div> Periksa kembali data pembayaran Anda sebelum melanjutkan transaksi</div>
+<div> Jika melalui teller, Isi Nama Pemilik Rekening dengan nama penyetor dan Nomor Rekening dengan : 0000</div>
+<div> Demi Keamanan transaksi Anda pastikan untuk tidak menginformasikan bukti dan data pembayaran</div>
+<br>
+<br>
+<div style="color: red;font-size: 15pt">WARNING!!!</div>
+<div>Begitu proses telah selesai transaksi tidak dapat direfund</div>
+
+<input type="hidden" id="btnSave" class="form-submit" style="margin-top:unset;margin-left:600px" value="Simpan" onclick="save();"/>
+</div>
+
+
+</div>
+</div>
+
+</div>
+        
         </section>
 
     </div>
